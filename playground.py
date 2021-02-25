4b176771dc77bb55da698bbde1cfee4af8fb78fa
qbxt <10813JJ@gmail.com>
2021-02-24 22:00:54 -0700

    first test of python

--- playground.py
+++ playground.py
@@ -1,29 +1,50 @@
+import os
+
 import discord
 from discord.ext import commands
 import re
 import docker
+import tarfile
+
 
 class Playground(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.docker = docker.from_env()
+        self.dockerHost = docker.from_env()
 
     @commands.Command(name="py", aliases=["python"])
     @commands.max_concurrency(1, per=commands.BucketType.user, wait=False)
     async def _py(self, ctx):
-        """Execute a small Python script and post the result to chat."""
-        code = ""
+        """Execute a Python snippet and post the result."""
+        async with ctx.typing():
+            code = ""
 
-        r = re.compile("```py(.*?)```")
-        match = r.search(ctx.message.content)
-        if match:
-            code = match.group(1)
-        else:
-            await ctx.send("Invalid syntax. Please use a Python codeblock (` ```py`).")
-            return
+            r = re.compile("```py(.*?)```")
+            match = r.search(ctx.message.content)
+            if match:
+                code = match.group(1)
+            else:
+                await ctx.send("Invalid syntax. Please use a Python code block (` ```py`).")
+                return
 
-        with open("{}.py".format(ctx.message.id), "w") as f:
-            f.write(code)
+            os.makedirs("playground", exist_ok=True)
+            with open("playground/{}.py".format(ctx.message.id), mode="w") as f:
+                f.write(code)
+            with tarfile.open("playground/{}.tar".format(ctx.message.id), mode="w") as tar:
+                tar.add("playground/{}.py".format(ctx.message.id))
 
-        # Start container
+            data = open("playground/{}.py".format(ctx.message.id), "rb").read()
 
+            # Start container
+            container = self.dockerHost.containers.create("python:3.9.2-alpine3.13", name=str(ctx.message.id))
+            container.put_archive("/tmp", data)
+
+            # Returns tuple of exit_code, output
+            exit_and_output = container.exec_run(["timeout", "60s", "python3.8", "{}.py".format(ctx.message.id)])
+
+            # Cleanup
+            container.remove()
+
+            with open("playground/{}.out".format(ctx.message.id), mode="w") as f:
+                f.write(exit_and_output[1])
+                await ctx.send("exit {}: ```\n{}```".format(exit_and_output[0], exit_and_output[1][0:800]), file=f)
