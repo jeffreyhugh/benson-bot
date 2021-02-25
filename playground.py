be1396ff04170b6c2a1909626ef58ac32b306a0a
qbxt <10813JJ@gmail.com>
2021-02-25 00:29:51 -0700

    python fully works

--- playground.py
+++ playground.py
@@ -1,4 +1,7 @@
+import asyncio
 import os
+import threading
+import time
 
 import discord
 from discord.ext import commands
@@ -7,61 +10,79 @@ import docker
 import tarfile
 
 
+def get_logs_from_container(container, save_name):
+    container.wait()
+
+    output = container.logs()
+
+    with open("playground/{}.out".format(save_name), mode="wb") as f:
+        f.write(output)
+
+
 class Playground(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.dockerHost = docker.from_env()
 
     @commands.command(name="py", aliases=["python"])
-    @commands.max_concurrency(1, per=commands.BucketType.user, wait=False)
+    @commands.max_concurrency(1, commands.BucketType.user, wait=False)
     async def _py(self, ctx):
         """Execute a Python snippet and post the result."""
         async with ctx.typing():
-            code = ""
-
-            r = re.compile("```(?:py|python)([^.]*?)```")
+            r = re.compile("```(?:python|py)([^.]*?)```")
             match = r.search(ctx.message.content)
             if match:
                 code = match.group(1)
             else:
+                await ctx.message.add_reaction("❌")
                 await ctx.send("Invalid syntax. Please use a Python code block (` ```py`).")
                 return
+
+            await ctx.message.add_reaction("⏳")
 
             os.makedirs("playground", exist_ok=True)
             with open("playground/{}.py".format(ctx.message.id), mode="w") as f:
                 f.write(code)
-            with tarfile.open("playground/{}.tar".format(ctx.message.id), mode="w") as tar:
-                tar.add("playground/{}.py".format(ctx.message.id))
 
-            data = open("playground/{}.tar".format(ctx.message.id), "rb").read()
+            exited_with_error = False
 
             # Build and start container
-            image = self.dockerHost.images.build(path="./",
-                                                 dockerfile="dockerfiles/python-Dockerfile",
-                                                 buildargs={"MESSAGE_ID": str(ctx.message.id)},
-                                                 tag="benson/" + str(ctx.message.id),
-                                                 forcerm=True)
-            output = self.dockerHost.containers.run("benson/" + str(ctx.message.id),
-                                                    name=str(ctx.message.id),
-                                                    auto_remove=True)
+            self.dockerHost.images.build(path="./",
+                                         dockerfile="dockerfiles/python-Dockerfile",
+                                         buildargs={"MESSAGE_ID": str(ctx.message.id)},
+                                         tag="benson/" + str(ctx.message.id),
+                                         forcerm=True)
 
-            # container.put_archive("/tmp", data)
+            container = self.dockerHost.containers.run("benson/" + str(ctx.message.id),
+                                                       name=str(ctx.message.id),
+                                                       auto_remove=False,
+                                                       stdout=True,
+                                                       stderr=True,
+                                                       detach=True)
 
-            # Returns tuple of exit_code, output
-            #while True:
-            #    try:
-            #        exit_and_output = container.exec_run(["python3", "/tmp/{}.py".format(ctx.message.id)])
-            #    except docker.errors.APIError:
-            #        continue
-            #    finally:
-            #        break
+            t = threading.Thread(target=get_logs_from_container, name=str(ctx.message.id), args=(container, ctx.message.id))
+            t.daemon = True
+            t.start()
 
+            os.remove("playground/{}.py".format(ctx.message.id))
 
-            # Cleanup
-            #container.kill()
-            #container.remove()
-            os.remove("playground/{}.*".format(ctx.message.id))
+            while not os.path.exists("playground/{}.out".format(ctx.message.id)):
+                await asyncio.sleep(1)
 
-            with open("playground/{}.out".format(ctx.message.id), mode="wb") as f:
-                f.write(output)
-                await ctx.send("__**Python 3.9**__ ```{}```".format(bytes.decode(output)), file=discord.File("playground/{}.out".format(ctx.message.id)))
+            with open("playground/{}.out".format(ctx.message.id)) as f:
+                output = f.read()
+
+            await ctx.send("<@{}> ```{}```".format(ctx.author.id, output[0:800]),
+                           file=discord.File("playground/{}.out".format(ctx.message.id)))
+
+            os.remove("playground/{}.out".format(ctx.message.id))
+
+        return
+
+    @_py.error
+    async def _py_error(self, ctx, error):
+        if isinstance(error, commands.errors.MaxConcurrencyReached):
+            await ctx.message.add_reaction("❌")
+            await ctx.send("You can only run one instance of this command at a time.")
+        else:
+            raise error
