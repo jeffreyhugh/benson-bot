2ad873cec4de55b6e14ae132f8b68862d5537ba5
qbxt <10813JJ@gmail.com>
2021-03-02 13:21:24 -0700

    update main w support for diff env

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import os
 
-import discord
 from discord.ext import commands
-
-from playground import Playground
 
 
 def main():
-    intents = discord.Intents.none()
-    intents.messages = True
-    intents.reactions = True
-    intents.guilds = True
-    #intents.members = True
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or("!!"))
 
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or("!!"), intents=intents)
+    env = os.getenv("BENSONBOT_ENV_TYPE")
+    if env == "MAIN":
+        from duty_manager import DutyManager
+        from memeail import ProblemOfTheDay
+        from shitposter import Shitposter
+        from ticker import Ticker
+        from timers import CSE107Timer, CSE113Timer
+        from message_scheduler import MessageScheduler
 
-    # Utility
-    bot.add_cog(Playground(bot))
+        bot.add_cog(CSE113Timer(bot))
+        bot.add_cog(CSE107Timer(bot))
+        bot.add_cog(DutyManager(bot))
+        bot.add_cog(MessageScheduler(bot))
+
+        bot.add_cog(ProblemOfTheDay(bot))
+
+        bot.add_cog(Shitposter(bot))
+        bot.add_cog(Ticker(bot))
+
+    elif env == "PLAYGROUND":
+        from playground import Playground
+
+        bot.add_cog(Playground(bot))
+
+    else:
+        print("Unknown environment ({})".format(env))
+        return
 
     bot.run(os.getenv("BENSONBOT_DISCORD_TOKEN"))
 
